73c849d163b633f4045ae2191f6de191c46d1fa8
Emmanuel Mortoo <emortoo@yahoo.com>
2023-10-02 19:06:30 -0400

    updates

--- Maven/maven-standalone-application/src/main/java/com/mt/sample/HelloWorld.java
+++ Maven/maven-standalone-application/src/main/java/com/mt/sample/HelloWorld.java
@@ -9,12 +9,12 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Engineers, Welcome to Landmark Technologies DevOps Master Class. You are sudying Maven Build Tool. !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello Engineers, Welcome to United Core system DevOps Bootcamp Class 2023. You are sudying Maven Build Tool. !! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
 	private static Date getLocalCurrentDate() {
-		String m = "Landmark Technologies";
+		String m = "United Core System";
 		return new Date();
 
 	}
