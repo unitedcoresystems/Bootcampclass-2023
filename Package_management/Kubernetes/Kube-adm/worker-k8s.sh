b5e04e94d5b3ee2b753a0db21771657e4d0961fb
Emmanuel Mortoo <63193071+emortoo@users.noreply.github.com>
2023-11-25 13:55:18 -0500

    Update worker-k8s.sh

--- Package_management/Kubernetes/Kube-adm/worker-k8s.sh
+++ Package_management/Kubernetes/Kube-adm/worker-k8s.sh
@@ -99,5 +99,6 @@ systemctl enable kubelet.service
 #Copy kubeadm join token from the master and execute in Worker Nodes to join to cluster
 # replace this token with yours
 
-kubeadm join 10.0.0.11:6443 --token 03em5o.agjpy9wbj98izbpn \
+<!-- kubeadm join 10.0.0.11:6443 --token 03em5o.agjpy9wbj98izbpn \
         --discovery-token-ca-cert-hash sha256:92a1cf1e452961a550f05eff32f25500fc9bcfd5d5f771689e8ee21b6a0da243
+-->
